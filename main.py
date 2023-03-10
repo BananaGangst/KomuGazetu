1f95fbef2d437c0c9fb6719f60c635e9268180f0
BananaGangst <109524578+BananaGangst@users.noreply.github.com>
2023-03-10 23:53:45 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import os, json, string
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import time
+from database import Database
+from config import API_TOKEN, ADMIN_ID1, ADMIN_ID2
+
+#ТОКЕН БОТА
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+database = Database("database.db")
+
+
+version = "1.0" ##НАПИШИ ВЕРСИЮ Пример : "1.0"
+
+
+async def on_startup(_):
+	print("Бот запущен!")
+
+
+@dp.message_handler(commands=['get_id'])
+async def get_id(message: types.Message):
+    await message.answer(message.from_user.id)
+
+###########   ОСНОВНАЯ ЧАСТЬ   #############
+
+
+#ФИЛЬТР И КОМАНДЫ
+@dp.message_handler()
+async def echo_send(message : types.Message):
+	if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}\
+		.intersection(set(json.load(open('cenz.json')))) != set():
+		await message.reply(f"🤬 Маты запрещены 🤬 \n@{message.from_user.username}")
+		await message.delete()
+  elif {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}\
+		.intersection(set(json.load(open('cenz1.json')))) != set():
+		await message.reply(f"🤬 Ссылки запрещены 🤬 \n@{message.from_user.username}")
+		await message.delete()
+
+
+
+#РАБОТА БОТА=TRUE
+#executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+if __name__ == '__main__':
+    executor.start_polling(dp)
